57b500907186a2acfcf6cee190124ff68c0e77cb
Melissa <melissa@espressolabs.com.br>
2020-04-07 16:32:12 -0300

    main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path')
 
 app.use(routes);
 app.use("/images", express.static(path.resolve(__dirname, "images")));
-app.listen(8080, ()=>{
+app.listen(process.env.PORT || 8080, () => {
 	console.log('check');
-	
+
 });
