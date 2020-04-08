7ec05b5608fae0f4bff24fb72d01ae39e5dd15ea
Melissa <melissa@espressolabs.com.br>
2020-04-08 19:02:06 -0300

    routes.js: devolvendo o data direto sem objeto

--- routes.js
+++ routes.js
@@ -2,29 +2,21 @@ const router = require('express').Router();
 const podcastJson = require('./podcasts');
 const episodesJson = require('./episodes');
 
-router.get('/podcasts', (req, res) =>{
-	res.send({
-		podcasts: podcastJson
-	})
+router.get('/podcasts', (req, res) => {
+	res.send(podcastJson)
 });
 
-router.get('/podcast/:id', (req, res) =>{
-	res.send({
-		podcast: podcastJson.filter(pod => pod.id == req.params.id)
-	})
+router.get('/podcast/:id', (req, res) => {
+	res.send(podcastJson.filter(pod => pod.id == req.params.id)
+	)
 });
 
-router.get('/episodes', (req, res) =>{
-	res.send({
-		episodes: episodesJson
-	})
+router.get('/episodes', (req, res) => {
+	res.send(episodesJson)
 });
 
-router.get('/episode/:id', (req, res) =>{
-	res.send({
-		episode: episodesJson.filter(ep => ep.id == req.params.id)
-	})
+router.get('/episode/:id', (req, res) => {
+	res.send(episodesJson.filter(ep => ep.id == req.params.id))
 });
-
 
 module.exports = router;
